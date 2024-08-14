edaac266974c919686a8a60745ca63e74a43bff6
Aldo Wiranata <148690939+alwiranata@users.noreply.github.com>
2024-08-14 17:21:23 +0700

    update

--- src/Content.js
+++ src/Content.js
@@ -14,7 +14,7 @@ import services_logo1 from "./assets/images/Services/logo1.png"
 import services_logo2 from "./assets/images/Services/logo2.png"
 import services_logo3 from "./assets/images/Services/logo3.png"
 
-import project1 from "./assets/images/projects/project2.jpeg"
+import project1 from "./assets/images/projects/project5.jpeg"
 import project2 from "./assets/images/projects/project1.jpeg"
 import project3 from "./assets/images/projects/project3.jpeg"
 import person_project from "./assets/images/projects/project4.png"
